e2b897232410a5f27bde137e8ab445bbcb2a5f12
Philip <phstaude@htwg-konstanz.de>
2012-06-20 18:33:52 +0200

    square controller + view

---
+++ application/controllers/SquareController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: phil
+ * Date: 20.06.12
+ * Time: 18:25
+ * To change this template use File | Settings | File Templates.
+ */
+
+class SquareController extends Zend_Controller_Action
+{
+    public function squareAction() {
+
+    }
+}
