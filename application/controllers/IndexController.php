aeec8e7481f03ad09b3e1e36b3528977568429f8
Pascal88 <palaier@htwg-konstanz.de>
2012-10-17 12:36:30 +0200

    cleaned repo added images

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -6,17 +6,4 @@
 class IndexController extends Zend_Controller_Action
 {
 
-    public function indexAction() {
-
-    }
-
-    public function loginAction() {
-        $this->layout()->header = $this->render("login");
-    }
-
-    public function appindexAction(){
-        $this->_helper->layout->setLayout('layout');
-    }
-
-
 }
--- application/controllers/MenuController.php
+++ application/controllers/MenuController.php
@@ -5,26 +5,5 @@
  */
 class MenuController extends Zend_Controller_Action
 {
-    public function init()
-    {
-        $this->_helper->layout->disableLayout();
-        $this->_helper->contextSwitch()
-            ->addActionContext('preferences', 'json')
-            ->initContext();
-    }
-
-    public function indexAction() {
-        $menuMapper = new Model_Menu_DbMapper();
-        $this->view->menus = $menuMapper->getMenu();
-    }
-
-    public function preferencesAction(){
-        if($this->getRequest()->isGet()){
-            var_dump($this->getRequest()->getParam("ff"));
-               $this->view->test = Zend_Json::encode(array("test" => "laber"));
-        } elseif ($this->getRequest()->isPost()){
-
-        }
-    }
 
 }
