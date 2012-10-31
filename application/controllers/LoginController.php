d63bbc0e620f02a50e5aeffaf25ecdcc587e5c0c
Sebastian <sebasguillen@gmail.com>
2012-10-31 22:04:09 +0100

    Tiny unstable changes, working on user authentication

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -6,20 +6,6 @@
 class LoginController extends Zend_Controller_Action
 {
     function indexAction() {
-        $this->render('index');
-    }
-
-    function loginAction() {
-        /*Welcome <?php echo $_POST["username"]; ?>!<br />
-        You are <?php echo $_POST["password"]; ?> years old.
-        You want to be remembered <?php echo $_POST["loginkeeping"]; ?>.*/
-
-        $request = $this->getRequest();
-        $this->view->assign('action', $request->getBaseURL()."/user/auth");
-        $this->view->assign('title', 'Login Form');
-        $this->view->assign('username', 'User Name');
-        $this->view->assign('password', 'Password');
-
-        //$this->_forward("index","load",null,null);
+        //$this->render('index');
     }
 }
