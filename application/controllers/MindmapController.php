1413c80f66f249ac1b82cc0c11dd44c6dda0e790
Philip <phstaude@htwg-konstanz.de>
2013-01-12 13:31:02 +0100

    login mindmap controller seperated

--- application/controllers/MindmapController.php
+++ application/controllers/MindmapController.php
@@ -8,13 +8,13 @@
 class MindmapController extends Zend_Controller_Action
 {
     public function indexAction(){
-        $storage = new Zend_Auth_Storage_Session();
+       /** $storage = new Zend_Auth_Storage_Session();
         $data = $storage->read();
         if(!$data){
             $this->_redirect('login/login');
         }
         $this->view->username = $data->username;
-        $this->render();
+        $this->render();*/
     }
 
 }
