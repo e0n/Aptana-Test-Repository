0c9bdb5fa0f6b23d456a4a27d1ae0e099532afc1
code-schreiber <code-schreiber@users.noreply.github.com>
2013-01-25 17:39:08 +0100

    Added Menu Buttons (New, Save, Load)

--- www/js/mindmap-debugGUI.js
+++ www/js/mindmap-debugGUI.js
@@ -145,5 +145,30 @@ debugGUI = {
         });
 
         return button;
+    },
+
+    buildNewMenuButton : function() {
+        var button = new Kinetic.Text({
+            stroke: '#555',
+            strokeWidth: 2,
+            fill: '#ddd',
+            text: 'tex',
+            fontSize: 14,
+            fontFamily: 'Calibri',
+            textFill: '#555',
+            width: 120,
+            padding: 20,
+            align: 'center',
+            fontStyle: 'italic',
+            cornerRadius: 5
+        });
+        // add cursor styling for shape oval
+        button.on("mouseover", function() {
+            document.body.style.cursor = "pointer";
+        });
+        button.on("mouseout", function() {
+            document.body.style.cursor = "default";
+        });
+        return button;
     }
 }
