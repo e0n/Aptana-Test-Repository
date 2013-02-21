cf350072eb90fa6f9972c1c16b44577fd5a5863e
code-schreiber <code-schreiber@users.noreply.github.com>
2013-02-21 19:15:33 +0100

    LoadController Doc

--- application/controllers/LoadController.php
+++ application/controllers/LoadController.php
@@ -1,20 +1,17 @@
 <?php
 /**
  * The active session will be controlled in this class.
- * The function indexAction checks the login status of the user.
- * When it is logged, the process goes on.
- * Otherwise login session will be generated and the login screen will be shown up.
- * 
- * Function nextsiteAction leads the user to the next site of the application
- * 
+ *
  * User: pascal
  * Date: 05.05.12
- * 
- * @param string
- * @param string
  */
 class LoadController extends Zend_Controller_Action
 {
+    /**
+    * Checks the login status of the user.
+    * When he is logged in, the process goes on.
+    * Otherwise he is redirected to the login screen.
+    */
     public function indexAction(){
         $storage = new Zend_Auth_Storage_Session();
         $data = $storage->read();
@@ -24,7 +21,9 @@ class LoadController extends Zend_Controller_Action
         $this->view->username = $data->username;
     }
 
-    //TODO this is only temporal, load heavy stuff instead
+    /**
+    * Leads the user to the next site of the application, the mindmap site.
+    */
     public function nextsiteAction(){
         $this->_redirect('mindmap');
     }
