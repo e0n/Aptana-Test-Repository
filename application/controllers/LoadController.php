c116a203a26a6c62868f6723e0e950a449c3c8cc
wiwin-klaar <wiwin-klaar@web.de>
2013-02-21 18:53:53 +0100

    Update application/controllers/LoadController.php

--- application/controllers/LoadController.php
+++ application/controllers/LoadController.php
@@ -1,7 +1,17 @@
 <?php
 /**
+ * The active session will be controlled in this class.
+ * The function indexAction checks the login status of the user.
+ * When it is logged, the process goes on.
+ * Otherwise login session will be generated and the login screen will be shown up.
+ * 
+ * Function nextsiteAction leads the user to the next site of the application
+ * 
  * User: pascal
  * Date: 05.05.12
+ * 
+ * @param string
+ * @param string
  */
 class LoadController extends Zend_Controller_Action
 {
