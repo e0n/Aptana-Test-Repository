6880e998c3fde8972cfce7e5a2613d094a9925af
Pascal88 <Pascal.laier@gmail.com>
2013-02-22 03:02:42 +0100

    mapmenu

--- www/js/mainMenuView.js
+++ www/js/mainMenuView.js
@@ -16,11 +16,11 @@ function setMenuText(val){
     if(val != ''){
         $(text).html(val);
         if(document.menuTextLeft == undefined) {
-            document.menuTextLeft = $(text).offset().left + 200;
+            document.menuTextLeft = $(text).offset().left + 240;
         }
         $(text).css('margin-left', document.menuTextLeft - ($(text).width() / 2));
         if(document.menuTextTop == undefined){
-            document.menuTextTop = $(text).offset().top + 80;
+            document.menuTextTop = $(text).offset().top + 90;
         }
         $(text).css('margin-top', document.menuTextTop - ($(text).height() / 2) - 130);
     } else {
